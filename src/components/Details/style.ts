36dfa320e67513548b926865d61196f857b00f56
pedro <phbs1235@gmail.com>
2020-11-12 22:03:44 -0300

    update styled

--- src/components/Details/style.ts
+++ src/components/Details/style.ts
@@ -1,59 +1,62 @@
 import { StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+    background-color: #fff;
+    height: 300px;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    border-width: 1px;
+    border-color: #ddd;
+    align-items: center;
+    padding: 20px;
+`;
+
+export const Title = styled.Text`
+    font-family: 'Poppins_600SemiBold';
+    font-size: 20px;
+    color: #222;
+`;
+
+export const Description = styled.Text`
+    font-family: 'Poppins_300Light';
+    font-size: 14px;
+    color: #666;
+`;
+
+export const ImageCar = styled.Image`
+    margin-top: 10px;
+    margin-bottom: 10px;
+    width: 80px;
+    height: 80px;
+`;
+
+export const RequestButton = styled(RectButton)`
+    height: 44px;
+    background-color: #222;
+    justify-content: center;
+    align-items: center;
+    align-self: stretch;
+    border-radius: 3px;
+    margin-top: 10px;
+`;
+
+export const RequestButtonText = styled.Text`
+    font-family: 'Poppins_600SemiBold';
+    font-size: 18px;
+    color: #fff;
+`;
 
 const styles = StyleSheet.create({
 
   container: {
-    backgroundColor: '#fff',
-    height: 300,
-    width: '100%',
-    position: 'absolute',
-    bottom: 0,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.2,
     elevation: 3,
-    borderWidth: 1,
-    borderColor: '#ddd',
-    alignItems: 'center',
-    padding: 20,
   },
-
-  typeTitle: {
-    // fontFamily: 'Poppins_600SemiBold',
-    fontSize: 20,
-    color: '#222'
-  },
-
-  typeDescription: {
-    // fontFamily: 'Poppins_300Light',
-    fontSize: 14,
-    color: '#666',
-  },
-
-  typeImage: {
-    height: 80,
-    width: 80,
-    marginVertical: 10
-  },
-
-  requestButton: {
-    height: 44,
-    backgroundColor: 'red',
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'stretch',
-    borderRadius: 3,
-    marginTop: 10,
-  },
-
-  requestButtonText: {
-    color: '#fff',
-    // fontFamily: 'Poppins_600SemiBold',
-    fontSize: 18
-  },
-
-
-
 });
 
 export default styles;
