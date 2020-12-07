e10aa1ae720eb1e43dd7b25e713a8d233bcae660
pedro <phbs1235@gmail.com>
2020-12-07 00:01:23 -0300

    create new pages

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,12 +1,20 @@
 import React, { useRef, useCallback, useState } from 'react';
 import {
-  View,Text, SafeAreaView, KeyboardAvoidingView, Platform, ScrollView, TextInput, Alert, ActivityIndicator,
+  View,
+  Text, 
+  SafeAreaView, 
+  KeyboardAvoidingView, 
+  Platform, 
+  ScrollView, 
+  TextInput, 
+  Alert, 
+  ActivityIndicator,
+  Picker
 } from 'react-native';
 import Icon from '@expo/vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
-import FlashMessage, { showMessage, hideMessage } from 'react-native-flash-message';
 import Input from '../../components/Input';
 
 import {
@@ -20,44 +28,58 @@ import {
   TextSignUp
 } from './styles';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import api from '../../utils/api';
 
 interface SignUpFormData {
   name: string;
-  lastName: string;
+  cpf: string;
   email: string;
-  bornDate: string;
   phone: string;
   password: string;
+  car: string;
+  car_plate: string;
+  cnh: string;
 }
 
 const SignUp: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false);
   const navigation = useNavigation();
   const formRef = useRef<FormHandles>(null);
-  const lastNameInputRef = useRef<TextInput>(null);
+  const cpfInputRef = useRef<TextInput>(null);
   const emailInputRef = useRef<TextInput>(null);
   const bornDateInputRef = useRef<TextInput>(null);
   const phoneInputRef = useRef<TextInput>(null);
   const passwordInputRef = useRef<TextInput>(null);
   const [date, setDate] = useState('');
   const [phone, setPhone] = useState('');
-
-
-  const handleSignUp = useCallback(
-    async (data: SignUpFormData) => {
+  const [typeUser, setTypeUser] = useState(0)
+  const handleSignUp = async (data: SignUpFormData) => {
       setIsLoading(true);
+      const { 
+        name,
+        email,
+        phone,
+        password,
+        cpf,
+        car,
+        car_plate,
+        cnh,
+      } = data
       try {
+          await api.post('/user/register', {
+            name, email, phone, password, cpf,car, car_plate,
+            cnh, type_user: typeUser
+          });
           navigation.navigate('SignIn');
           setIsLoading(false);
           alert(`Cadastro efetuado com sucesso!`);
 
       } catch (error) {
+        console.log(error)
         setIsLoading(false);
         alert(`Ocorreu um erro ao fazer o cadastro, tente novamente.`)
       }
-    },
-    [navigation],
-  );
+    }
 
 
   return (
@@ -68,15 +90,15 @@ const SignUp: React.FC = () => {
       behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     >
       <SafeAreaView style={{ flex: 1 }}>
-        <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 90, marginBottom: -20 }}>
-          <TextSignUp>Faça seu cadastro</TextSignUp>
-        </View>
         <ListOptions
           contentContainerStyle={Platform.OS === 'web' ? { flex: 1 } : undefined}
           keyboardShouldPersistTaps="handled"
         >
           <Container>
-            <FlashMessage/>
+            
+            <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 90, marginBottom: -20 }}>
+              <TextSignUp>Faça seu cadastro</TextSignUp>
+            </View>
             <Form ref={formRef} onSubmit={handleSignUp} style={{ marginTop: 50 }}>
               <Input
                 autoCapitalize="words"
@@ -85,15 +107,15 @@ const SignUp: React.FC = () => {
                 placeholder="Nome"
                 returnKeyType="next"
                 onSubmitEditing={() => (
-                lastNameInputRef.current?.focus()
+                cpfInputRef.current?.focus()
                 )}
               />
 
               <Input
                 autoCapitalize="words"
                 autoCorrect={false}
-                name="lastName"
-                placeholder="Sobrenome"
+                name="cpf"
+                placeholder="CPF"
                 returnKeyType="next"
                 onSubmitEditing={() => (
                 emailInputRef.current?.focus()
@@ -114,20 +136,6 @@ const SignUp: React.FC = () => {
               <Input
                 autoCapitalize="words"
                 autoCorrect={false}
-                name="bornDate"
-                value={date}
-                mask="date"
-                placeholder="Data de nascimento"
-                returnKeyType="next"
-                keyboardType="numbers-and-punctuation"
-                inputMaskChange={(text: string) => setDate(text)}
-                onSubmitEditing={() => (
-                phoneInputRef.current?.focus()
-                )}
-              />
-              <Input
-                autoCapitalize="words"
-                autoCorrect={false}
                 name="phone"
                 mask="phone"
                 value={phone}
@@ -136,10 +144,40 @@ const SignUp: React.FC = () => {
                 placeholder="Telefone"
                 
                 returnKeyType="next"
-                onSubmitEditing={() => (
-                passwordInputRef.current?.focus()
-                )}
               />
+              <Picker
+                  selectedValue={typeUser}
+                  onValueChange={(itemValue, itemIndex) => setTypeUser(itemValue)}
+              >
+                <Picker.Item label="Escolha seu perfil" value="" />
+                <Picker.Item label="Passageiro" value="1" />
+                <Picker.Item label="Motorista" value="2" />
+              </Picker>
+              
+              {
+                Number(typeUser) === 2 ? 
+                <>
+                  <Input
+                  ref={passwordInputRef}
+                  name="car"
+                  placeholder="Qual seu carro?"
+                  />
+                  <Input
+                  ref={passwordInputRef}
+                  name="car_plate"
+                  placeholder="Placa do carro"
+                  />
+                  <Input
+                  ref={passwordInputRef}
+                  name="cnh"
+                  placeholder="Sua CNH"
+                  />
+                </>
+                :
+                <View>
+                  <Text/>
+                </View>
+              }
               <Input
                 ref={passwordInputRef}
                 secureTextEntry
@@ -152,13 +190,11 @@ const SignUp: React.FC = () => {
               <ContainerColor>
                 <ContainerButton onPress={() => formRef.current?.submitForm()}>
                   <TextButton>
-                  Cadastrar
+                    Cadastrar
                   </TextButton>
                 </ContainerButton>
               </ContainerColor>
-              
             </Form>
-
             <CreateAccountButton onPress={() => navigation.goBack()}>
               <Icon name="arrow-left" size={20} color="#C4C4C4" />
               <CreateAccountButtonText>
